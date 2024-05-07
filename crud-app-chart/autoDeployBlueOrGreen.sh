f01fd3eaefabba561483e9e47ce6b57c0ff805d3
joschne <jonas.schneider@balcab.ch>
2024-05-07 17:28:44 +0200

    update script

--- crud-app-chart/autoDeployBlueOrGreen.sh
+++ crud-app-chart/autoDeployBlueOrGreen.sh
@@ -45,6 +45,28 @@ yq -i .$newSlot.enabled=true values.yaml
 yq -i .$newSlot.appVersion=$newAppVersion values.yaml
 yq -i '.productionSlot="'"$oldSlot"'"' values.yaml
 
+# commit and push changes
+git add values.yaml && git commit -m "update values.yaml" && git push
+
+# apply changes to k8s cluster
+bash upgradeChart.sh
+
+echo "##############################################################"
+echo "Deployed to slot $newSlot with version: $newAppVersion"
+echo "Switching production slot from $oldSlot to $newSlot"
+echo "##############################################################"
+
+
+# update values.yaml
+yq -i .$oldSlot.enabled=false values.yaml
+yq -i '.productionSlot="'"$newSlot"'"' values.yaml
+
+# commit and push changes
+git add values.yaml && git commit -m "update values.yaml" && git push
+
+# apply changes to k8s cluster
+bash upgradeChart.sh
+
 # # deploy new app version on new slot
 # helm upgrade crud-app . --install --atomic --wait --reset-then-reuse-values=true --debug --timeout 5m00s -n joschne-dev \
 #     --set $newSlot.enabled=true \
